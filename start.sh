5b66c74a2cfad251268bf5eb934ec9ba0678633f
Kederf-8 <napolig05@gmail.com>
2024-09-24 23:22:36 +0200

    Split builds and pushs and fix start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 # Build and push Docker images
-cd Backend
-docker build -f Dockerfile.backend -t gcr.io/solarcaroncloud/backend:latest .
+docker build -f Backend/Dockerfile -t gcr.io/solarcaroncloud/backend:latest .
 docker push gcr.io/solarcaroncloud/backend:latest
 
-cd ../Frontend
-docker build -f Dockerfile.frontend -t gcr.io/solarcaroncloud/frontend:latest .
+docker build -f Frontend/Dockerfile -t gcr.io/solarcaroncloud/frontend:latest .
 docker push gcr.io/solarcaroncloud/frontend:latest
 
-cd ../Data-Sender
-docker build -f Dockerfile.data-sender -t gcr.io/solarcaroncloud/data-sender:latest .
+docker build -f Data-Sender/Dockerfile -t gcr.io/solarcaroncloud/data-sender:latest .
 docker push gcr.io/solarcaroncloud/data-sender:latest
 
 # Apply Kubernetes deployments
-cd ..
 kubectl apply -f backend-deployment.yaml
 kubectl apply -f frontend-deployment.yaml
 kubectl apply -f sender-deployment.yaml
